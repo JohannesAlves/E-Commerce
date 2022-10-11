7a521d2dcf399b1130305e17c6b05d5edaec5628
Jeff-Dev88 <74569799+Jeff-Dev88@users.noreply.github.com>
2022-10-11 15:51:27 -0300

    Adicionado cards dos produtos e sua reponsividade
    Demais alterações: Favicon e titulo da pagina

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import CardProduct from "../components/CardProduct";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { Wrap, WrapItem, Center } from "@chakra-ui/react";
 
 export default function Home() {
   return (
@@ -7,6 +9,28 @@ export default function Home() {
       <Navbar />
       <div>
         <h1>Olá, essa é a nossa HomePage</h1>
+        <Wrap>
+          <WrapItem w="400px" gap="1">
+            <Center>
+              <CardProduct />
+            </Center>
+          </WrapItem>
+          <WrapItem w="400px" gap="1">
+            <Center>
+              <CardProduct />
+            </Center>
+          </WrapItem>
+          <WrapItem w="400px" gap="1">
+            <Center>
+              <CardProduct />
+            </Center>
+          </WrapItem>
+          <WrapItem w="400px" gap="1">
+            <Center>
+              <CardProduct />
+            </Center>
+          </WrapItem>
+        </Wrap>
       </div>
       <Footer />
     </>
